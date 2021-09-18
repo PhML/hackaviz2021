05061bd6eeb4a5370c423eb8e32af6a19ee7a714
Philippe Mallet-Ladeira <philippe.mallet-ladeira@csgroup.eu>
2021-09-18 18:31:46 +0200

    refact: use template from www.d3-graph-gallery.com

--- script.js
+++ script.js
@@ -1,76 +1,66 @@
-const margin = { top: 40, bottom: 10, left: 120, right: 20 };
+const margin = { top: 40, bottom: 100, left: 120, right: 20 };
 const width = 800 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
 
-d3.csv("pop_qp.csv", d3.autoType).then((csv) => {
-  data = csv;
-  draw(data);
-});
+// Parse the Data
+d3.csv("pop_commune.csv", d3.autoType).then((data) =>
+  draw(data, "nom_commune", "pop_mun")
+);
 
-const draw = (data) => {
-  // Creates sources <svg> element
+const draw = (data, xName, yName) => {
+  // append the svg object to the body of the page
   const svg = d3
     .select("#BarChart")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
-
-  x = d3
-    .scaleBand()
-    .domain(data.map((d) => d.nom_commune))
-    .range([margin.left, width - margin.right])
-    .padding(0.1);
-  y = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, (d) => d.pop_mun)])
-    .nice()
-    .range([height - margin.bottom, margin.top]);
-
-  xAxis = (g) =>
-    g
-      .attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(x).tickSizeOuter(0));
-
-  yAxis = (g) =>
-    g
-      .attr("transform", `translate(${margin.left},0)`)
-      .call(d3.axisLeft(y))
-      .call((g) => g.select(".domain").remove());
-
-  // height = 500;
-  // margin = { top: 20, right: 0, bottom: 30, left: 40 };
-
-  const bar = svg
+    .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("fill", "steelblue")
-    .selectAll("rect")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  // X axis
+  const x = d3
+    .scaleBand()
+    .range([0, width])
+    .domain(data.map((d) => d[xName]))
+    .padding(0.2);
+
+  svg
+    .append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+
+  // Add Y axis
+  const y = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d[yName])])
+    .nice()
+    .range([height, 0]);
+
+  svg.append("g").call(d3.axisLeft(y));
+
+  // Bars
+  svg
+    .selectAll("mybar")
     .data(data)
     .join("rect")
-    .style("mix-blend-mode", "multiply")
-    .attr("x", (d) => x(d.nom_commune))
-    .attr("y", (d) => y(d.pop_mun))
-    .attr("height", (d) => y(0) - y(d.pop_mun))
-    .attr("width", x.bandwidth());
+    .attr("x", (d) => x(d[xName]))
+    .attr("width", x.bandwidth())
+    .attr("fill", "#69b3a2")
+    // no bar at the beginning thus:
+    .attr("height", (d) => height - y(0)) // always equal to 0
+    .attr("y", (d) => y(0));
 
-  const gx = svg.append("g").call(xAxis);
-
-  const gy = svg.append("g").call(yAxis);
-
-  const update = (order) => {
-    x.domain(data.sort(order).map((d) => d.nom_commune));
-
-    const t = svg.transition().duration(750);
-
-    bar
-      .data(data, (d) => d.nom_commune)
-      .order()
-      .transition(t)
-      .delay((d, i) => i * 20)
-      .attr("x", (d) => x(d.nom_commune));
-
-    gx.transition(t)
-      .call(xAxis)
-      .selectAll(".tick")
-      .delay((d, i) => i * 20);
-  };
+  // Animation
+  svg
+    .selectAll("rect")
+    .transition()
+    .duration(800)
+    .attr("y", (d) => y(d[yName]))
+    .attr("height", (d) => height - y(d[yName]))
+    .delay((d, i) => {
+      return i * 10;
+    });
 };
